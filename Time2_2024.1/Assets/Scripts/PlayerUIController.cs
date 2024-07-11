886bca6a298f90fe79b3f7308cafa3c3e60cc5a8
Iquecs2005 <iquecarvalho2005@gmail.com>
2024-07-11 16:34:23 -0300

    Animacação UI

--- Time2_2024.1/Assets/Scripts/PlayerUIController.cs
+++ Time2_2024.1/Assets/Scripts/PlayerUIController.cs
@@ -12,15 +12,20 @@ public class PlayerUIController : MonoBehaviour
     [SerializeField] private TMP_Text attackText;
     [SerializeField] private TMP_Text attackSpeedText;
     [SerializeField] private Image itemImage;
+    [SerializeField] public Animator CaveiraoAnim;
     [SerializeField] private Sprite[] spriteVector;
  
-    public void UpdateUI(float maxHealth, float health, float attackDamage, float attackSpeed, int selectedItem) 
+    public void UpdateUI(float maxHealth, float health, float attackDamage, float attackSpeed) 
     {
         healthBar.maxValue = maxHealth;
         healthBar.value = health;
         healthText.text = $"{health}/{maxHealth}";
         attackText.text = attackDamage.ToString();
         attackSpeedText.text = attackSpeed.ToString();
+    }
+
+    public void UpdateItem(int selectedItem)
+    {
         itemImage.sprite = spriteVector[selectedItem];
     }
 }
