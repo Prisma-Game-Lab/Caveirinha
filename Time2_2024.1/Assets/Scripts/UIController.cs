f36198e9fd5289b10b025c25a675b4a7829a8bf7
jomigmonte <jomigmonte@gmail.com>
2024-07-17 01:38:53 -0300

    slider de áudio funcional no menu de pause

--- Time2_2024.1/Assets/Scripts/UIController.cs
+++ Time2_2024.1/Assets/Scripts/UIController.cs
@@ -6,14 +6,24 @@ public class UIController : MonoBehaviour
 {
     //Comentario
     public Slider _musicSlider, _sfxSlider;
+    public AudioSource music, sfx;
 
     public void MusicVolume()
     {
         AudioManager.instance.MusicVolume(_musicSlider.value);
+        music.volume = _musicSlider.value;
+        
     }
 
     public void SFXVolume()
     {
         AudioManager.instance.SFXVolume(_sfxSlider.value);
+        sfx.volume = _sfxSlider.value;
+    }
+
+    private void Update()
+    {
+        MusicVolume();
+        SFXVolume();
     }
 }
